b89da051601cbaa9b9c7396b42be15d5d839ec07
Thiago Camelo <ti.thiagocamelo@gmail.com>
2020-05-12 10:02:02 -0300

    segunda tentativa

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.revendalight.com.br/nagalli/admin/');
+define('HTTP_CATALOG', 'http://www.revendalight.com.br/nagalli/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://www.revendalight.com.br/nagalli/admin/');
+define('HTTPS_CATALOG', 'https://www.revendalight.com.br/nagalli/');
+
+// DIR
+define('DIR_APPLICATION', '/home2/metaf215/public_html/nagalli/admin/');
+define('DIR_SYSTEM', '/home2/metaf215/public_html/nagalli/system/');
+define('DIR_IMAGE', '/home2/metaf215/public_html/nagalli/image/');
+define('DIR_LANGUAGE', '/home2/metaf215/public_html/nagalli/admin/language/');
+define('DIR_TEMPLATE', '/home2/metaf215/public_html/nagalli/admin/view/template/');
+define('DIR_CONFIG', '/home2/metaf215/public_html/nagalli/system/config/');
+define('DIR_CACHE', '/home2/metaf215/public_html/nagalli/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home2/metaf215/public_html/nagalli/system/storage/download/');
+define('DIR_LOGS', '/home2/metaf215/public_html/nagalli/system/storage/logs/');
+define('DIR_MODIFICATION', '/home2/metaf215/public_html/nagalli/system/storage/modification/');
+define('DIR_UPLOAD', '/home2/metaf215/public_html/nagalli/system/storage/upload/');
+define('DIR_CATALOG', '/home2/metaf215/public_html/nagalli/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'metaf215_loris');
+define('DB_PASSWORD', 'f!7-ar8=60Bc');
+define('DB_DATABASE', 'metaf215_lori');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.revendalight.com.br/nagalli/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://www.revendalight.com.br/nagalli/');
+
+// DIR
+define('DIR_ROOT', '/home2/metaf215/public_html/nagalli/');
+define('DIR_APPLICATION', '/home2/metaf215/public_html/nagalli/catalog/');
+define('DIR_SYSTEM', '/home2/metaf215/public_html/nagalli/system/');
+define('DIR_IMAGE', '/home2/metaf215/public_html/nagalli/image/');
+define('DIR_LANGUAGE', '/home2/metaf215/public_html/nagalli/catalog/language/');
+define('DIR_TEMPLATE', '/home2/metaf215/public_html/nagalli/catalog/view/theme/');
+define('DIR_CONFIG', '/home2/metaf215/public_html/nagalli/system/config/');
+define('DIR_CACHE', '/home2/metaf215/public_html/nagalli/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home2/metaf215/public_html/nagalli/system/storage/download/');
+define('DIR_LOGS', '/home2/metaf215/public_html/nagalli/system/storage/logs/');
+define('DIR_MODIFICATION', '/home2/metaf215/public_html/nagalli/system/storage/modification/');
+define('DIR_UPLOAD', '/home2/metaf215/public_html/nagalli/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'metaf215_loris');
+define('DB_PASSWORD', 'f!7-ar8=60Bc');
+define('DB_DATABASE', 'metaf215_lori');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
