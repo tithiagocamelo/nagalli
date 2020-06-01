292b320404b5e5ac69030d46089b8632f170ca10
Thiago Camelo <ti.thiagocamelo@gmail.com>
2020-06-01 09:23:54 -0300

    teste

--- admin/config.php
+++ admin/config.php
@@ -1,25 +1,25 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://www.revendalight.com.br/nagalli/admin/');
-define('HTTP_CATALOG', 'http://www.revendalight.com.br/nagalli/');
+define('HTTP_SERVER', 'http://www.revendalight.com.br/nagali1/admin/');
+define('HTTP_CATALOG', 'http://www.revendalight.com.br/nagali1/');
 
 // HTTPS
-define('HTTPS_SERVER', 'https://www.revendalight.com.br/nagalli/admin/');
-define('HTTPS_CATALOG', 'https://www.revendalight.com.br/nagalli/');
+define('HTTPS_SERVER', 'https://www.revendalight.com.br/nagali1/admin/');
+define('HTTPS_CATALOG', 'https://www.revendalight.com.br/nagali1/');
 
 // DIR
-define('DIR_APPLICATION', '/home2/metaf215/public_html/nagalli/admin/');
-define('DIR_SYSTEM', '/home2/metaf215/public_html/nagalli/system/');
-define('DIR_IMAGE', '/home2/metaf215/public_html/nagalli/image/');
-define('DIR_LANGUAGE', '/home2/metaf215/public_html/nagalli/admin/language/');
-define('DIR_TEMPLATE', '/home2/metaf215/public_html/nagalli/admin/view/template/');
-define('DIR_CONFIG', '/home2/metaf215/public_html/nagalli/system/config/');
-define('DIR_CACHE', '/home2/metaf215/public_html/nagalli/system/storage/cache/');
-define('DIR_DOWNLOAD', '/home2/metaf215/public_html/nagalli/system/storage/download/');
-define('DIR_LOGS', '/home2/metaf215/public_html/nagalli/system/storage/logs/');
-define('DIR_MODIFICATION', '/home2/metaf215/public_html/nagalli/system/storage/modification/');
-define('DIR_UPLOAD', '/home2/metaf215/public_html/nagalli/system/storage/upload/');
-define('DIR_CATALOG', '/home2/metaf215/public_html/nagalli/catalog/');
+define('DIR_APPLICATION', '/home2/metaf215/public_html/nagali1/admin/');
+define('DIR_SYSTEM', '/home2/metaf215/public_html/nagali1/system/');
+define('DIR_IMAGE', '/home2/metaf215/public_html/nagali1/image/');
+define('DIR_LANGUAGE', '/home2/metaf215/public_html/nagali1/admin/language/');
+define('DIR_TEMPLATE', '/home2/metaf215/public_html/nagali1/admin/view/template/');
+define('DIR_CONFIG', '/home2/metaf215/public_html/nagali1/system/config/');
+define('DIR_CACHE', '/home2/metaf215/public_html/nagali1/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home2/metaf215/public_html/nagali1/system/storage/download/');
+define('DIR_LOGS', '/home2/metaf215/public_html/nagali1/system/storage/logs/');
+define('DIR_MODIFICATION', '/home2/metaf215/public_html/nagali1/system/storage/modification/');
+define('DIR_UPLOAD', '/home2/metaf215/public_html/nagali1/system/storage/upload/');
+define('DIR_CATALOG', '/home2/metaf215/public_html/nagali1/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
