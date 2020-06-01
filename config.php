86e763e49673e794720184e54231bcf6d9800d41
Thiago Camelo <ti.thiagocamelo@gmail.com>
2020-06-01 09:19:20 -0300

    teste1

--- config.php
+++ config.php
@@ -1,23 +1,23 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://www.revendalight.com.br/nagalli/');
+define('HTTP_SERVER', 'http://www.revendalight.com.br/nagali1/');
 
 // HTTPS
-define('HTTPS_SERVER', 'https://www.revendalight.com.br/nagalli/');
+define('HTTPS_SERVER', 'https://www.revendalight.com.br/nagali1/');
 
 // DIR
-define('DIR_ROOT', '/home2/metaf215/public_html/nagalli/');
-define('DIR_APPLICATION', '/home2/metaf215/public_html/nagalli/catalog/');
-define('DIR_SYSTEM', '/home2/metaf215/public_html/nagalli/system/');
-define('DIR_IMAGE', '/home2/metaf215/public_html/nagalli/image/');
-define('DIR_LANGUAGE', '/home2/metaf215/public_html/nagalli/catalog/language/');
-define('DIR_TEMPLATE', '/home2/metaf215/public_html/nagalli/catalog/view/theme/');
-define('DIR_CONFIG', '/home2/metaf215/public_html/nagalli/system/config/');
-define('DIR_CACHE', '/home2/metaf215/public_html/nagalli/system/storage/cache/');
-define('DIR_DOWNLOAD', '/home2/metaf215/public_html/nagalli/system/storage/download/');
-define('DIR_LOGS', '/home2/metaf215/public_html/nagalli/system/storage/logs/');
-define('DIR_MODIFICATION', '/home2/metaf215/public_html/nagalli/system/storage/modification/');
-define('DIR_UPLOAD', '/home2/metaf215/public_html/nagalli/system/storage/upload/');
+define('DIR_ROOT', '/home2/metaf215/public_html/nagali1/');
+define('DIR_APPLICATION', '/home2/metaf215/public_html/nagali1/catalog/');
+define('DIR_SYSTEM', '/home2/metaf215/public_html/nagali1/system/');
+define('DIR_IMAGE', '/home2/metaf215/public_html/nagali1/image/');
+define('DIR_LANGUAGE', '/home2/metaf215/public_html/nagali1/catalog/language/');
+define('DIR_TEMPLATE', '/home2/metaf215/public_html/nagali1/catalog/view/theme/');
+define('DIR_CONFIG', '/home2/metaf215/public_html/nagali1/system/config/');
+define('DIR_CACHE', '/home2/metaf215/public_html/nagali1/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home2/metaf215/public_html/nagali1/system/storage/download/');
+define('DIR_LOGS', '/home2/metaf215/public_html/nagali1/system/storage/logs/');
+define('DIR_MODIFICATION', '/home2/metaf215/public_html/nagali1/system/storage/modification/');
+define('DIR_UPLOAD', '/home2/metaf215/public_html/nagali1/system/storage/upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
